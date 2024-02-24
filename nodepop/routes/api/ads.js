4e6c10677845d06ec194b5d7fb3d6155ed9ac28e
Santiagomadaw <santiagoma.daw@gmail.com>
2024-02-24 13:16:24 +0100

    Añadido filtro de precio

--- nodepop/routes/api/ads.js
+++ nodepop/routes/api/ads.js
@@ -7,10 +7,39 @@ router.get('/', async function(req, res, next) {
 
 try {
     const filterBySell = req.query.sell
+    const minprice = req.query.min
+    const maxprice = req.query.max
+
+
     const start = req.query.start
     const step = req.query.step
+
+
     const filter = {}
+
+if (filterBySell){
     filter.sell=filterBySell
+    console.log(filterBySell)
+
+}
+if (minprice){ 
+    filter.price= { $gt: minprice }
+    console.log(filter)
+
+}
+if (maxprice){ 
+    filter.price= { $lt: maxprice }
+    console.log(filter)
+
+}
+if (minprice && maxprice){ 
+    filter.price= { $gt: minprice,$lt: maxprice }
+    console.log(filter)
+
+}
+
+
+
     const ads = await Ad.show(filter, start, step)
     res.json({result:ads});
 } catch (error) {
