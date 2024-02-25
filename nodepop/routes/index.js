72281d8c31ae6451933891e650bc0272c5a7d87f
Santiagomadaw <santiagoma.daw@gmail.com>
2024-02-25 10:51:35 +0100

    Primera version del view index.ejs

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const Ad =require('../model/Ads');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Titulo' });
+router.get('/', async function(req, res, next) {
+  const data = await Ad.find()
+  const title = 'Nodepop'
+  res.render('index', {title, data });
 });
 
 module.exports = router;
